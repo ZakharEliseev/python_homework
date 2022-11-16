e52e5dbd21f812f3b4684bf7f5f87ddd2a7c0b58
ezv <eliseev.zv@gmail.com>
2022-11-16 16:16:51 +0500

    /

--- homework_02/car.py
+++ homework_02/car.py
@@ -2,13 +2,16 @@
 создайте класс `Car`, наследник `Vehicle`
 """
 from dataclasses import dataclass
+
+from docker.api import volume
+
 from homework_02.base import Vehicle
 from homework_02.engine import Engine
 
 
 @dataclass(Engine)
 class Car(Vehicle):
-    engine = Engine()
+    engine = None
 
     def __init__(self, weight=0, fuel=0, fuel_consumption=0):
         super().__init__(weight=0, fuel=0, fuel_consumption=0, )
